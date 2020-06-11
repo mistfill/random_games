49c001921fd8f2ab5dc275d28d841b6172b0358c
mistfill <58302058+mistfill@users.noreply.github.com>
2020-06-11 22:02:24 +0600

    Add files via upload

---
+++ ssm.js
@@ -0,0 +1,347 @@
+function showButtonSsecondBox(){
+    let hover_button_second_box = document.getElementById('hover_button_second_box');
+
+    hover_button_second_box.style.opacity = 1;
+}
+
+function hideButtonSsecondBox(){
+    let hover_button_second_box = document.getElementById('hover_button_second_box');
+
+    hover_button_second_box.style.opacity = 0;
+}
+
+function showMusicHoverBox(){
+    let MusicHoverBox = document.getElementById('musicHoverContBox4');
+
+    MusicHoverBox.style.opacity = 1;
+}
+
+function hideMusicHoverBox(){
+    let MusicHoverBox = document.getElementById('musicHoverContBox4');
+
+    MusicHoverBox.style.opacity = 0;
+}
+
+function showMusicHoverBoxMin(){
+    let MusicHoverBox = document.getElementById('musicHoverContBox4Min');
+
+    MusicHoverBox.style.opacity = 1;
+}
+
+function hideMusicHoverBoxMin(){
+    let MusicHoverBox = document.getElementById('musicHoverContBox4Min');
+
+    MusicHoverBox.style.opacity = 0;
+}
+
+
+
+
+
+function showShowHoverBox(){
+    let ShowHoverContBox4 = document.getElementById('ShowHoverContBox4');
+
+    ShowHoverContBox4.style.opacity = 1;
+}
+
+function hideShowHoverBox(){
+    let ShowHoverContBox4 = document.getElementById('ShowHoverContBox4');
+
+    ShowHoverContBox4.style.opacity = 0;
+}
+
+function showShowHoverBoxMin(){
+    let ShowHoverContBox4Min = document.getElementById('ShowHoverContBox4Min');
+
+    ShowHoverContBox4Min.style.opacity = 1;
+}
+
+function hideShowHoverBoxMin(){
+    let ShowHoverContBox4Min = document.getElementById('ShowHoverContBox4Min');
+
+    ShowHoverContBox4Min.style.opacity = 0;
+}
+
+
+
+
+function showKidsContBox(){
+    let KidsHoverBox = document.getElementById('KidsHoverBox');
+
+    KidsHoverBox.style.opacity = 1;
+}
+
+function hideKidsContBox(){
+    let KidsHoverBox = document.getElementById('KidsHoverBox');
+
+    KidsHoverBox.style.opacity = 0;
+}
+
+function showKidsContBoxMin(){
+    let KidsHoverBoxMin = document.getElementById('KidsHoverBoxMin');
+
+    KidsHoverBoxMin.style.opacity = 1;
+}
+
+function hideKidsContBoxMin(){
+    let KidsHoverBoxMin = document.getElementById('KidsHoverBoxMin');
+
+    KidsHoverBoxMin.style.opacity = 0;
+}
+
+
+
+function showScetchContBox(){
+    let ScetchHoverBox = document.getElementById('ScetchHoverBox');
+
+    ScetchHoverBox.style.opacity = 1;
+}
+
+function hideScetchContBox(){
+    let ScetchHoverBox = document.getElementById('ScetchHoverBox');
+
+    ScetchHoverBox.style.opacity = 0;
+}
+
+function showScetchContBoxMin(){
+    let ScetchHoverBoxMin = document.getElementById('ScetchHoverBoxMin');
+
+    ScetchHoverBoxMin.style.opacity = 1;
+}
+
+function hideScetchContBoxMin(){
+    let ScetchHoverBoxMin = document.getElementById('ScetchHoverBoxMin');
+
+    ScetchHoverBoxMin.style.opacity = 0;
+}
+
+
+
+
+function showSportContBox(){
+    let SportHoverBox = document.getElementById('SportHoverBox');
+
+    SportHoverBox.style.opacity = 1;
+}
+
+function hideSportContBox(){
+    let SportHoverBox = document.getElementById('SportHoverBox');
+
+    SportHoverBox.style.opacity = 0;
+}
+
+function showSportContBoxMin(){
+    let SportHoverBoxMin = document.getElementById('SportHoverBoxMin');
+
+    SportHoverBoxMin.style.opacity = 1;
+}
+
+function hideSportContBoxMin(){
+    let SportHoverBoxMin = document.getElementById('SportHoverBoxMin');
+
+    SportHoverBoxMin.style.opacity = 0;
+}
+
+
+
+
+function showHealthContBox(){
+    let HealthHoverBox = document.getElementById('HealthHoverBox');
+
+    HealthHoverBox.style.opacity = 1;
+}
+
+function hideHealthContBox(){
+    let HealthHoverBox = document.getElementById('HealthHoverBox');
+
+    HealthHoverBox.style.opacity = 0;
+}
+
+function showHealthContBoxMin(){
+    let HealthHoverBoxMin = document.getElementById('HealthHoverBoxMin');
+
+    HealthHoverBoxMin.style.opacity = 1;
+}
+
+function hideHealthContBoxMin(){
+    let HealthHoverBoxMin = document.getElementById('HealthHoverBoxMin');
+
+    HealthHoverBoxMin.style.opacity = 0;
+}
+
+
+
+
+
+function showSeriesContBox(){
+    let SeriesHoverBox = document.getElementById('SeriesHoverBox');
+
+    SeriesHoverBox.style.opacity = 1;
+}
+
+function hideSeriesContBox(){
+    let SeriesHoverBox = document.getElementById('SeriesHoverBox');
+
+    SeriesHoverBox.style.opacity = 0;
+}
+
+function showSeriesContBoxMin(){
+    let SeriesHoverBoxMin = document.getElementById('SeriesHoverBoxMin');
+
+    SeriesHoverBoxMin.style.opacity = 1;
+}
+
+function hideSeriesContBoxMin(){
+    let SeriesHoverBoxMin = document.getElementById('SeriesHoverBoxMin');
+
+    SeriesHoverBoxMin.style.opacity = 0;
+}
+
+
+
+
+
+function showInterviewContBox(){
+    let InterveiwHoverBox = document.getElementById('InterveiwHoverBox');
+
+    InterveiwHoverBox.style.opacity = 1;
+}
+
+function hideInterviewContBox(){
+    let InterveiwHoverBox = document.getElementById('InterveiwHoverBox');
+
+    InterveiwHoverBox.style.opacity = 0;
+}
+
+function showInterviewContBoxMin(){
+    let InterveiwHoverBoxMin = document.getElementById('InterveiwHoverBoxMin');
+
+    InterveiwHoverBoxMin.style.opacity = 1;
+}
+
+function hideInterviewContBoxMin(){
+    let InterveiwHoverBoxMin = document.getElementById('InterveiwHoverBoxMin');
+
+    InterveiwHoverBoxMin.style.opacity = 0;
+}
+
+
+
+
+function showSocialContBox(){
+    let SocialHoverBox = document.getElementById('SocialHoverBox');
+
+    SocialHoverBox.style.opacity = 1;
+}
+
+function hideSocialContBox(){
+    let SocialHoverBox = document.getElementById('SocialHoverBox');
+
+    SocialHoverBox.style.opacity = 0;
+}
+
+function showSocialContBoxMin(){
+    let SocialHoverBoxMin = document.getElementById('SocialHoverBoxMin');
+
+    SocialHoverBoxMin.style.opacity = 1;
+}
+
+function hideSocialContBoxMin(){
+    let SocialHoverBoxMin = document.getElementById('SocialHoverBoxMin');
+
+    SocialHoverBoxMin.style.opacity = 0;
+}
+
+
+
+function showPodcastContBox(){
+    let PodcastHoverBox = document.getElementById('PodcastHoverBox');
+
+    PodcastHoverBox.style.opacity = 1;
+}
+
+function hidePodcastContBox(){
+    let PodcastHoverBox = document.getElementById('PodcastHoverBox');
+
+    PodcastHoverBox.style.opacity = 0;
+}
+
+function showPodcastContBoxMin(){
+    let PodcastHoverBoxMin = document.getElementById('PodcastHoverBoxMin');
+
+    PodcastHoverBoxMin.style.opacity = 1;
+}
+
+function hidePodcastContBoxMin(){
+    let PodcastHoverBoxMin = document.getElementById('PodcastHoverBoxMin');
+
+    PodcastHoverBoxMin.style.opacity = 0;
+}
+
+
+
+
+
+function showCyberContBox(){
+    let CyberHoverBox = document.getElementById('CyberHoverBox');
+
+    CyberHoverBox.style.opacity = 1;
+}
+
+function hideCyberContBox(){
+    let CyberHoverBox = document.getElementById('CyberHoverBox');
+
+    CyberHoverBox.style.opacity = 0;
+}
+
+function showCyberContBoxMin(){
+    let CyberHoverBoxMin = document.getElementById('CyberHoverBoxMin');
+
+    CyberHoverBoxMin.style.opacity = 1;
+}
+
+function hideCyberContBoxMin(){
+    let CyberHoverBoxMin = document.getElementById('CyberHoverBoxMin');
+
+    CyberHoverBoxMin.style.opacity = 0;
+}
+
+
+function showAdresHoverPadcontBox9(){
+    let AdresHoverPadcontBox9 = document.getElementById('AdresHoverPadcontBox9');
+
+    AdresHoverPadcontBox9.style.opacity = 1;
+}
+
+function hideAdresHoverPadcontBox9(){
+    let AdresHoverPadcontBox9 = document.getElementById('AdresHoverPadcontBox9');
+
+    AdresHoverPadcontBox9.style.opacity = 0;
+}
+
+
+
+let LolHandcontBox9 = document.getElementById('LolHandcontBox9');
+
+LolHandcontBox9.onmousedown = function(e){
+    LolHandcontBox9.style.position = 'absolute';
+    moveAt(e);
+    document.body.appendChild(LolHandcontBox9);
+
+    LolHandcontBox9.style.zIndex = 1000;
+
+    function moveAt(e){
+        LolHandcontBox9.style.left = e.pageX - LolHandcontBox9.offsetWidth / 2 + 'px';
+        LolHandcontBox9.style.top = e.pageY - LolHandcontBox9.offsetHeight / 4 + 'px';
+    }
+
+    document.onmousemove = function(e){
+        moveAt(e);
+    }
+
+    LolHandcontBox9.onmouseup = function(){
+        document.onmousemove = null;
+        LolHandcontBox9.onmouseup = null;
+    }
+
+}
