9c9c0d29635d95e51b54f5b153e14e00b34033bd
mistfill <58302058+mistfill@users.noreply.github.com>
2020-10-19 15:55:18 +0600

    Update welcome_page_damdi_v19102020.js

--- welcome_page_damdi_v19102020.js
+++ welcome_page_damdi_v19102020.js
@@ -1,66 +1,7 @@
 
 let menu_a = 1;
 
-/*function plusClick(){
-    /*console.log(menu_a)
-    let one = document.getElementById('one');
-    let oneClass = one.className;
-    
-    if (oneClass == 'one') {
-        one.className = 'test1'
-    } 
-    else {
-        one.className = 'one';
-    }
-
-    let two = document.getElementById('two');
-    let twoClass = two.className;
-
-    if (twoClass == 'two') {
-        two.className = 'test2'
-    } 
-    else {
-        two.className = 'two';
-    }
-
-    let menu_box = document.getElementById('menu_box');
-    let menu_towns = document.getElementById('menu_towns');
-
-    if (menu_a == 1) {
-        menu_box.style.height = '150px';
-        menu_a = 2;
-        menu_towns.style.display = 'unset';
-    }
-    else {
-        menu_box.style.height = '75px'
-        menu_a = 1;
-        menu_towns.style.display = 'none';
-    }
-}*/
-
 bbh_id = 0;
-
-/*function showCitys() {
-    let big_button_hide = document.getElementById('big_button_hide');
-    let city_box = document.getElementById('city_box');
-
-    if (bbh_id == 0){
-        big_button_hide.style.opacity = 1;
-        big_button_hide.style.cursor = 'pointer';
-        city_box.style.top = '60vh';
-        city_box.style.display = 'flex';
-    }
-}
-
-function hideCitys(){
-    let big_button_hide = document.getElementById('big_button_hide');
-    big_button_hide.style.opacity = 0;
-    big_button_hide.style.cursor = 'unset';
-
-    let city_box = document.getElementById('city_box');
-    city_box.style.top = '120vh';
-    city_box.style.display = 'none';
-}*/
 
 function showCitys(){
     let start = Date.now();
